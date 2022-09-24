f32cacb34984896b91e9e91966b19bbbf773716e
Anne <edmondson.anne@gmail.com>
2022-09-24 11:17:03 -0600

    commit tree af0b47aa4afd9bf3a6e8ca7cec5e553c525e6e52

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength){
+  //let rideLength;
+  if (rideLength > 2500) {
+    return 'No can do.';
+  } else if (rideLength >= 2001 && rideLength <= 2499)  { 
+      return 'I will gladly take your thirty bucks.';
+    }
+    else if (rideLength >= 401 && rideLength <= 2000){
+      return 'That will be twenty bucks.'
+    }
+    else if (rideLength <= 400) {
+      return 'This one is on me!'
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(coolCity){
+  let myAnswer;
+  coolCity === 'NYC' ? myAnswer = 'Ok, sounds good.' :  myAnswer = 'No go.';
+  return myAnswer;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipReceived){
+  let myReply;
+  switch (tipReceived){
+    case 'generous':
+      myReply = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      myReply = 'Thank you.';
+      break;
+    default:
+      myReply = 'Bye.'
+      break;
+  }
+
+  return myReply;
 }
+
+
+
+/*
+
+function switchOnCharmFromTip(tipAmount){
+  // Write your code here!
+	switch (tipAmount) {
+		case 'generous' : returnResponse = 'Thank you so much.'; break;
+		case 'not as generous': returnResponse = 'Thank you.'; break;
+		default : returnResponse = 'Bye.';
+	}
+	return returnResponse;
+}
+ */
